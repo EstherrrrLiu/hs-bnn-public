d2191cfa441f48fb45fb235fb8e869a545fa35c2
Chang Liu <69181047+EstherrrrLiu@users.noreply.github.com>
2022-02-14 21:06:52 -0800

    Update plot_weights.py

--- src/plot_weights.py
+++ src/plot_weights.py
@@ -53,7 +53,7 @@ def plot_singlelayer_weights(mlp, posterior_mode=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("r_path")
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     r_path =args.r_path
     model = joblib.load(r_path)
     plot_singlelayer_weights(model)
